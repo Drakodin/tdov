29bb95d365c1ab22e516e2a65e63ce6dc593aabf
Drakodin <talia.yl.tian@gmail.com>
2023-03-30 22:33:37 -0500

    Add README.md, implement TDOV date check.

---
+++ docs/README.md
@@ -0,0 +1,8 @@
+## What does TDOV mean?
+Trans Day of Visibility!
+
+### What is this?
+It will look like a blank site for every day except TDOV (March 31). On TDOV, it will change into its active variant with words of encouragement, links to resources, and other useful and/or cheerful things.
+
+### Can't Wait? Activate it yourself!
+It's possible to see all of this w/o it being TDOV by calling the child function with a date that refers to March 31. It does consume UNIX timestamps so do note this.
---
+++ scripts/tdov.js
@@ -0,0 +1,33 @@
+const checkTime = (time = null) => {
+    let date = new Date();
+    if (time) {
+        date = new Date(time);
+    }
+
+    // Using browser local time
+    let day = date.getDate();
+    let month = date.getMonth();
+
+    console.log(date)
+
+    if (!(day === 31 && month === 2)) {
+        let body = document.querySelector("body")
+
+        if (body) {
+            let content = body.innerHTML
+            localStorage.setItem("webContent", content)
+            document.querySelector("html").removeChild(body)
+        }
+    } else {
+        let content = localStorage.getItem("webContent")
+        if (content) {
+            let body = document.createElement("body")
+            let html = document.querySelector("html")
+            body.innerHTML = content
+            html.appendChild(body)
+        }
+    }
+}
+
+// Run immediately, this is the only function in this JS file and it will be included in the <head>
+window.onload = () => checkTime()
